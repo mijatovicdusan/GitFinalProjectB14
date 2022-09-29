c548f9cd2e552e44193418501ad7fd2ec19a2987
mijatovicdusan <mijatovicdusan777@gmail.com>
2022-09-28 20:11:10 -0400

    first commit

---
+++ src/FinalProject/FinalClass.java
@@ -0,0 +1,30 @@
+package FinalProject;
+
+public class FinalClass {
+	public static void main(String[]args) {
+		
+		
+	System.out.println("this is the code for mercedes");	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
