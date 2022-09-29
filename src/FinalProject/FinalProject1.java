87d9d85e2871bb0bc830c44dd174188326e213c0
mijatovicdusan <mijatovicdusan777@gmail.com>
2022-09-28 20:48:22 -0400

    second commit

---
+++ src/FinalProject/FinalProject1.java
@@ -0,0 +1,28 @@
+package FinalProject;
+
+public class FinalProject1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("this is the code for audi");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
